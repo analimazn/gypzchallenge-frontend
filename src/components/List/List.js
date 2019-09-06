2ea5ab2ce16c87c00c0b7d4edb260de51f5ac62c
analimazn <analimazn@gmail.com>
2019-09-06 13:05:22 -0300

    Render informations about orders into List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,37 +1,85 @@
 import React from 'react'
-import { Container, Card, Button, Title, Text } from './ListStyle'
+import axios from 'axios'
+import { Container, Title, Button, Alert } from './ListStyle'
+import { Card } from '../Card/Card'
 
 export class List extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await this.getOrders()
+      if (res !== undefined) {
+        this.setState({ orders: res.data.data })
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async getOrders() {
+    try {
+      const response = await axios.get('http://0.0.0.0:3000/order')
+      if (response.status !== 200) throw Error(response.message)
+      return response
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async deleteOrder(order) {
+    if (window.confirm('Deseja mesmo remover esta solicitação?')) {
+      const res = await axios.delete('http://0.0.0.0:3000/order', 
+        { params: order })
+      const orders = await axios.get('http://0.0.0.0:3000/order')
+      if (orders !== undefined) {
+        this.setState({ orders: orders.data.data })
+      } else {
+        alert("Erro ao tentar deletar, por favor, tente novamente")
+      }
+    }
+  }
+
   render() {
     return (
       <Container>
-         <Card>
-          <Title>ananna:</Title>
-          <Text>ananna</Text>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-        </Card>
-          <Button>Remover</Button>
-        <Card>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-        </Card>
-          <Button>Remover</Button>
-        <Card>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-          <Title>OLÁ:</Title>
-          <Text>OLÁ</Text>
-        </Card>
-          <Button>Remover</Button>
+        {this.state.orders && this.state.orders.map(value => {
+          return (
+            <Container key={value._id}>
+              <Card
+                firstName={value.user.firstName}
+                lastName={value.user.lastName}
+                document={value.user.document}
+                bornDate={value.user.bornDate}
+                cellphoneNumber={value.contact.cellphoneNumber}
+                country={value.address.country}
+                df={value.address.df}
+                county={value.address.county}
+                postCode={value.address.postCode}
+                neighborhood={value.address.neighborhood}
+                street={value.address.street}
+                houseNumber={value.address.houseNumber}
+                moreInfo={value.address.moreInfo}
+                approved={value.creditCardInfo.approved}
+                creditLimit={value.creditCardInfo.creditLimit}
+                amount={value.creditCardInfo.amount}
+              >
+              </Card>
+              <Button
+                onClick={() => this.deleteOrder(value)}
+              >Remover</Button>
+            </Container>
+          )
+        })}
+        {!this.state.orders.length && 
+        <Alert>
+          <Title color='red'>Sem solicitações</Title>
+        </Alert>}
       </Container>
     )
   }
--- src/components/List/ListStyle.js
+++ src/components/List/ListStyle.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   align-content: center;
 `
 
-export const Card = styled.div`
+export const Alert = styled.div`
   display: flex;
   background-color: #FFF;
   flex-direction: column; 
@@ -15,7 +15,7 @@ export const Card = styled.div`
   margin: 10px 0 10px 0;
   border-radius: 5px;
   border: 2px solid #c9cffa;
-  text-align: justify;
+  text-align: center;
   
   @media (max-width: 570px) {
     width: 320px;
@@ -28,10 +28,10 @@ export const Card = styled.div`
   @media (min-width: 750px) {
     width: 710px;
   }
-  `
-  
-  export const Button = styled.button`
-  width: 320px;
+`
+
+export const Button = styled.button`
+  width: 200px;
   height: 35px;
   background: #3a83a5;
   color: #fff;
@@ -39,29 +39,12 @@ export const Card = styled.div`
   border: none;
   border-radius: 3px;
   align-self: center;
-  margin: 0 0 10px 0;
+  margin: 10px 0 10px 0;
 `
 
-export const Label = styled.label`
-  display: flex;
-  flex-direction: column;
-  color: #777;
-  font-family: "Raleway", sans-serif;
-  font-size: 0.8em;
-  margin: 0.5em 0;
-  position: relative;
-`
-
-export const Title = styled.h3`
-  font-family: 'Raleway', sans-serif;
-  font-weight: 300;
-  color: #4d4d4d;
-  text-transform: capitalize;
-`
-
-export const Text = styled.p`
+export const Title = styled.p`
   font-family: 'Raleway', sans-serif;
   text-indent: 10px;
-  text-transform: capitalize;
+  text-transform: uppercase;
   color: ${props => props.color || '#4d4d4d'}
 `
