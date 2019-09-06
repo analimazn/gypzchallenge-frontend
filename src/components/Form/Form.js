0ffaeff9d6d15f2f29b9d519789ffae810b9f48e
analimazn <analimazn@gmail.com>
2019-09-06 19:17:17 -0300

    Added function to save data

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,244 +1,279 @@
 import React from 'react'
 import moment from 'moment'
-import * as Yup from 'yup'
-import { Container, Form, Input, Button, Text, Label } from './FormStyle'
+import axios from 'axios'
+import { formValidationSchema } from '../../config/FormValidation'
+import { Container, Form, Input, Button, Alert, Text, Label } from './FormStyle'
 import { Formik } from 'formik'
 
 
-const formValidationSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(3, 'Mínimo 3 caracteres')
-    .max(30, 'Máximo 30 caracteres')
-    .trim()
-    .uppercase()
-    .required('Por favor, insira o seu primeiro nome'),
-  lastName: Yup.string()
-    .min(3, 'Mínimo 3 caracteres')
-    .max(30, 'Máximo 30 caracteres')
-    .trim()
-    .uppercase()
-    .required('Por favor, insira o seu sobrenome'),
-  document: Yup.number()
-    .positive('Por favor, insira valores inteiros')
-    .test('len', 'Mínimo 11 caracteres', doc => doc && doc.toString().length === 11)
-    .required('Por favor, insira o seu CPF'),
-  bornDate: Yup.date(),
-  zipCode: Yup.number()
-    .positive('Por favor, insira valores inteiros')
-    .test('len', 'Mínimo 8 caracteres', zip => zip && zip.toString().length === 8)
-    .required('Por favor, insira o seu CEP'),
-  houseNumber: Yup.number()
-    .positive('Por favor, insira valores inteiros')
-    .required('Por favor, insira o número da sua residência'),
-  moreInfo: Yup.string()
-    .trim()
-    .uppercase(),
-  cellphoneNumber: Yup.number()
-    .positive('Por favor, insira valores inteiros')
-    .test('len', 'Mínimo 11 caracteres', cell => cell && cell.toString().length === 11)
-    .required('Por favor, insira o número do seu celular'),
-  phoneNumber: Yup.number()
-    .positive('Por favor, insira valores inteiros')
-    .test('len', 'Mínimo 10 caracteres', phone => phone && phone.toString().length === 10),
-  amount: Yup.number()
-    .positive('Por favor, insira valores inteiros')
-    .min(100, 'Mínimo R$ 100,00')
-    .required('Por favor, insira a sua renda mensal')
-})
-
-
 export class CadasterCard extends React.Component {
-  componentDidUpdate(prevProps, prevState) {
-    this.formik.resetForm()
+  constructor(props) {
+    super(props)
+    this.state = {
+      showScreen: true
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const connection = await this.getIndex()
+      if (connection === undefined) {
+        this.setState({ showScreen: false })
+        throw Error(connection.message)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async getIndex() {
+    try {
+      const result = await axios.get('http://0.0.0.0:3000/')
+      if (result.status !== 200) {
+        throw Error(result.message)
+      }
+      return result
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   render() {
     return (
       <Container>
-        <Formik
-          ref={(ref) => this.formik = ref}
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            document: '',
-            bornDate: '',
-            zipCode: '',
-            houseNumber: '',
-            moreInfo: '',
-            cellphoneNumber: '',
-            phoneNumber: '',
-            amount: ''
-          }}
-          validate={ values => {
-            let errors = {}
-            let age = moment.duration(moment().diff(values.bornDate)).asYears()
-            
-            if (!values.bornDate) {
-              errors.bornDate = "Data de nascimento é obrigatória"
-            } else if (age < 18) {
-              errors.bornDate = 'Mínimo 18 anos'
-            }
-            return errors
-          }}
-          onSubmit={ values => {
-            alert(values, 2, null)
-          }}
-          onReset={ values => {
+        {!this.state.showScreen && 
+          <Alert> Ocorreu um erro, por favor tente novamente mais tarde :( </Alert>
+        }
+        {this.state.showScreen &&
+          <Formik
+            ref={(ref) => this.formik = ref}
+            initialValues={{
+              firstName: '',
+              lastName: '',
+              document: '',
+              bornDate: '',
+              cellphoneNumber: '',
+              country: '',
+              df: '',
+              county: '',
+              postCode: '',
+              neighborhood: '',
+              street: '',
+              houseNumber: '',
+              moreInfo: '',
+              amount: ''
+            }}
+            validate={values => {
+              let errors = {}
+              let age = moment.duration(moment().diff(values.bornDate)).asYears()
 
-          }}
-          validationSchema={ formValidationSchema }
-          render={({
-            touched,
-            errors,
-            values,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            handleReset
-          }) => (
-            <Form onSubmit={ handleSubmit }>
-              <Label>
-                Nome *
-                { errors.firstName && <Text color="red">{ errors.firstName }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.firstName || '' }
-                  border={ errors.firstName && "1px solid red" }
-                  type="text" 
-                  name="firstName" 
-                />
-              </Label>
-                
-              <Label>
-                Sobrenome *
-                { errors.lastName && <Text color="red">{ errors.lastName }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.lastName || '' }
-                  border={ errors.lastName && "1px solid red" }
-                  type="text"
-                  name="lastName"
-                />
-              </Label>
-                
-              <Label>
-                CPF *
-                { errors.document && <Text color="red">{ errors.document }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value= { values.document || '' }
-                  border={ errors.document && "1px solid red" }
-                  type="number"
-                  name="document"
-                  placeholder="Somente números"
-                />
-              </Label>
-              
-              <Label>
-                Data de Nascimento *
-                { errors.bornDate && <Text color="red">{ errors.bornDate }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.bornDate || '' }
-                  border={ errors.bornDate && "1px solid red" }
-                  type="date"
-                  name="bornDate"
-                />
-              </Label>
-                
-              <Label>
-                CEP *
-                { errors.zipCode && <Text color="red">{ errors.zipCode }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.zipCode || '' }
-                  border={ errors.zipCode && "1px solid red" }
-                  type="number"
-                  name="zipCode"
-                  placeholder="Somente números"
-                />
-              </Label>
-                
-              <Label>
-                Número da residência *
-                { errors.houseNumber && <Text color="red">{ errors.houseNumber }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.houseNumber || '' }
-                  border={ errors.houseNumber && "1px solid red" }
-                  type="number"
-                  name="houseNumber"
-                  placeholder="Somente números"
-                />
-              </Label>
+              if (!values.bornDate) {
+                errors.bornDate = "Data de nascimento é obrigatória"
+              } else if (age < 18) {
+                errors.bornDate = 'Mínimo 18 anos'
+              }
 
-              <Label>
-                Complemento
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.moreInfo || '' }
-                  type="text"
-                  name="moreInfo"
-                />
-              </Label>
-              
-              <Label>
-                Celular *
-                { errors.cellphoneNumber && <Text color="red">{ errors.cellphoneNumber }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.cellphoneNumber || '' }
-                  border={ errors.cellphoneNumber && "1px solid red" }
-                  type="number"
-                  name="cellphoneNumber"
-                  placeholder="Somente números (DDD + Número)"
-                />
-              </Label>
+              return errors
+            }}
+            onSubmit={ async (values, bag) => {
+              try {
+                const result = await axios.post('http://0.0.0.0:3000/order', { data: values })
+                if (result === undefined) {
+                  throw Error(result.message)
+                }
+                alert('Dados salvos com sucesso')
+                bag.resetForm()
+              } catch (err) {
+                console.error(err)
+              }
+            }}
+            validationSchema={formValidationSchema}
+            render={({
+              touched,
+              errors,
+              values,
+              handleChange,
+              handleBlur,
+              handleSubmit
+            }) => (
+                <Form onSubmit={handleSubmit}>
+                  <Label>
+                    Nome *
+                    {errors.firstName && <Text color="red">{errors.firstName}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.firstName || ''}
+                      border={errors.firstName && "1px solid red"}
+                      type="text"
+                      name="firstName"
+                    />
+                  </Label>
 
-              <Label>
-                Telefone
-                { errors.phoneNumber && <Text color="red">{ errors.phoneNumber }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.phoneNumber || '' }
-                  border={ errors.phoneNumber && "1px solid red" }
-                  type="number"
-                  name="phoneNumber"
-                  placeholder="Somente números (DDD + Número)"
-                />
-              </Label>
-              
-              <Label>
-                Renda *
-                { errors.amount && <Text color="red">{ errors.amount }</Text> }
-                <Input
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  value={ values.amount || '' }
-                  border={ errors.amount && "1px solid red" }
-                  type="number"
-                  name="amount"
-                  placeholder="Somente números (R$ 100,00)"
-                />
-              </Label>
-              
-              <Button 
-                type="submit" 
-                onReset={ handleReset }
-              > Enviar
-              </Button>
-            </Form>
-          )}
-        />
+                  <Label>
+                    Sobrenome *
+                    {errors.lastName && <Text color="red">{errors.lastName}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.lastName || ''}
+                      border={errors.lastName && "1px solid red"}
+                      type="text"
+                      name="lastName"
+                    />
+                  </Label>
+
+                  <Label>
+                    CPF *
+                    {errors.document && <Text color="red">{errors.document}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.document || ''}
+                      border={errors.document && "1px solid red"}
+                      type="number"
+                      name="document"
+                      placeholder="Somente números"
+                    />
+                  </Label>
+
+                  <Label>
+                    Data de Nascimento *
+                    {errors.bornDate && <Text color="red">{errors.bornDate}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.bornDate || ''}
+                      border={errors.bornDate && "1px solid red"}
+                      type="date"
+                      name="bornDate"
+                    />
+                  </Label>
+
+                  <Label>
+                    Celular *
+                    {errors.cellphoneNumber && <Text color="red">{errors.cellphoneNumber}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.cellphoneNumber || ''}
+                      border={errors.cellphoneNumber && "1px solid red"}
+                      type="number"
+                      name="cellphoneNumber"
+                      placeholder="Somente números (DDD + Número)"
+                    />
+                  </Label>
+
+                  <Label>
+                    Renda *
+                    {errors.amount && <Text color="red">{errors.amount}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.amount || ''}
+                      border={errors.amount && "1px solid red"}
+                      type="number"
+                      name="amount"
+                      placeholder="Somente números (R$ 100,00)"
+                    />
+                  </Label>
+
+                  <Label>
+                    País *
+                    {errors.country && <Text color="red">{errors.country}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.country || ''}
+                      type="text"
+                      name="country"
+                    />
+                  </Label>
+
+                  <Label>
+                    UF *
+                    {errors.df && <Text color="red">{errors.df}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.df || ''}
+                      type="text"
+                      name="df"
+                    />
+                  </Label>
+
+                  <Label>
+                    Cidade *
+                    {errors.county && <Text color="red">{errors.county}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.county || ''}
+                      type="text"
+                      name="county"
+                    />
+                  </Label>
+
+                  <Label>
+                    CEP *
+                  {errors.postCode && <Text color="red">{errors.postCode}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.postCode || ''}
+                      border={errors.postCode && "1px solid red"}
+                      type="number"
+                      name="postCode"
+                      placeholder="Somente números"
+                    />
+                  </Label>
+
+                  <Label>
+                    Bairro *
+                    {errors.neighborhood && <Text color="red">{errors.neighborhood}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.neighborhood || ''}
+                      type="text"
+                      name="neighborhood"
+                    />
+                  </Label>
+
+                  <Label>
+                    Rua *
+                    {errors.street && <Text color="red">{errors.street}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.street || ''}
+                      type="text"
+                      name="street"
+                    />
+                  </Label>
+
+                  <Label>
+                    Número da residência *
+                    {errors.houseNumber && <Text color="red">{errors.houseNumber}</Text>}
+                    <Input
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.houseNumber || ''}
+                      border={errors.houseNumber && "1px solid red"}
+                      type="number"
+                      name="houseNumber"
+                      placeholder="Somente números"
+                    />
+                  </Label>
+
+                  <Button
+                    type="submit"
+                  > Enviar
+                </Button>
+                </Form>
+              )}
+          />
+        }
       </Container>
     )
   }
--- src/components/Form/FormStyle.js
+++ src/components/Form/FormStyle.js
@@ -6,25 +6,17 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
 `
-
-export const Form = styled.form`
-  width: 800px;
-  margin: 0 0 20px 0;
-  padding: 10px;
-  background-color: #FFF;
+export const Alert = styled.div`
   display: flex;
-  flex-direction: column;
-  align-content: center;
-  align-items: center;
+  background-color: #FFF;
+  flex-direction: column; 
+  align-self: center;
+  padding: 10px;
+  margin: 10px 0 10px 0;
   border-radius: 5px;
-`
-
-export const Input = styled.input`
-  padding: 0.5em;
-  margin: 0.5em;
   border: 2px solid #c9cffa;
-  border-radius: 3px;
-
+  text-align: center;
+  
   @media (max-width: 570px) {
     width: 320px;
   }
@@ -38,8 +30,51 @@ export const Input = styled.input`
   }
 `
 
+export const Form = styled.form`
+  width: 800px;
+  margin: 0 0 20px 0;
+  padding: 10px;
+  background-color: #FFF;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  border-radius: 5px;
+  
+  @media (max-width: 570px) {
+    width: 330px;
+  }
+
+  @media (min-width: 570px) and (max-width: 750px) {
+    width: 560px;
+  } 
+
+  @media (min-width: 750px) {
+    width: 720px;
+  }
+`
+
+export const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  border: 2px solid #c9cffa;
+  border-radius: 3px;
+
+  @media (max-width: 570px) {
+    width: 300px;
+  }
+
+  @media (min-width: 570px) and (max-width: 750px) {
+    width: 520px;
+  } 
+
+  @media (min-width: 750px) {
+    width: 690px;
+  }
+`
+
 export const Button = styled.button`
-  width: 320px;
+  width: 200px;
   height: 35px;
   background: #3a83a5;
   color: #fff;
@@ -67,11 +102,11 @@ export const Title = styled.h1`
   font-size: 2.2em;
 `
 
-export const Title2 = styled.h2`
+export const Title3 = styled.h3`
   font-family: 'Raleway', sans-serif;
-  font-weight: 300;
   color: #4d4d4d;
-  font-size: 1.8em;
+  align-self: start;
+  margin: 10px 0 10px 10px;
 `
 
 export const Text = styled.p`
